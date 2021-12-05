629b44f7ac6a18267c5b592d917b571966ea5b42
J Krbaio <krbaio3@gmail.com>
2021-12-05 12:17:23 +0100

    Jenkins

---
+++ CI/groovy/BasicDocker.groovy
@@ -0,0 +1,8 @@
+String name = "jonh Doe"
+int courseCount = 14
+BigDecimal salary = 9999999999.99
+Boolean isProgrammer = true
+
+println name + " has created " + courseCount + " courses"
+println name + " is a programmer? " + isProgrammer
+println name + " whishes his salary was ${salary}"
